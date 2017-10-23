0c690dffaf785a2a78334597ad83c68d6aa6cd72
Yamuna Navada <yamunanavada@gmail.com>
2017-10-23 20:03:07 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,14 +1,13 @@
-require pry
+
 def unsafe?(speed)
 	if speed < 40 || speed > 60
-		binding.pry
 		return true
 	else
 		return false
+	end
 end
 
 
-
-#def not_safe?(speed)
-	#speed > 60 ? return true : return false end
-#end
+def not_safe?(speed)
+	speed < 40 || speed > 60 ? return true : return false end
+end
